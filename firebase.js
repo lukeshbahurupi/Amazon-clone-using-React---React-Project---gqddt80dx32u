7354e8f2486195361bde33c13202876fbaecd59b
lukeshbahurupi <lbahurupi567@gmail.com>
2022-12-05 14:47:12 +0530

    s

--- firebase.js
+++
@@ -1,9 +0,0 @@
-import firebase from "firebase";
-const firebaseConfig = {
-  apiKey: "AIzaSyC6aqNeZsBS-ptQurat9a6_3L2LC-HWgas",
-  authDomain: "amzn-1-cln.firebaseapp.com",
-  projectId: "amzn-1-cln",
-  storageBucket: "amzn-1-cln.appspot.com",
-  messagingSenderId: "1049388058159",
-  appId: "1:1049388058159:web:f80ba6b6da373a616da8da",
-};
--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import basketReducer from "../slices/basketSlice";
-
-//THE GLOBAL STORE
-export const store = configureStore({
-  reducer: {
-    basket: basketReducer,
-  },
-});
